50dc02af8301f15fa9c6e000d44c57d813e58cf1
Zeeshan Shafique <rmzeeshan188@gmail.com>
2018-05-06 04:31:23 +0500

    Users create and read pages are working except photo field

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,35 @@
-<h1> Admin Users Index Page</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>Users</h1>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Role</th>
+                <th>Active</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+          </tr>
+        </thead>
+        <tbody>
+            @if($users)
+                @foreach($users as $user)
+                    <tr>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{$user->role->name}}</td>
+                        <td>{{$user->is_active == 1 ? 'Active': 'Not Active'}}</td>
+                        <td>{{$user->created_at->diffForHumans()}}</td>
+                        <td>{{$user->updated_at->diffForHumans()}}</td>
+                    </tr>
+                @endforeach
+            @endif
+        </tbody>
+    </table>
+
+@stop
